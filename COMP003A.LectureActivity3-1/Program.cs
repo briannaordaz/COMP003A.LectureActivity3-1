ca1a503978fd2910b7e1b4ee63797dd260405962
briannaordaz <briannaordaz5@gmail.com>
2025-01-27 20:59:11 -0800

    added prompt for current hour

---
+++ COMP003A.LectureActivity3-1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP003A.LectureActivity3_1;
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Selection Statements Demo!");
+        
+        Console.Write("Enter the current hour (0-23): ");
+        int hour = int.Parse(Console.ReadLine());
+    }
+}
